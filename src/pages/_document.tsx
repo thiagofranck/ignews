9e1971d01458e1b188dd6942dd488948b6de680e
Thiago Franck <thiagohenrique776@gmail.com>
2021-04-22 15:48:18 -0300

    Componente Header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,700;0,900;1,300&display=swap" rel="stylesheet" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
         </Head>
         <body>
           <Main />

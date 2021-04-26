2d7f71ca025e8ff4669bd7069baae74565d76137
Thiago Franck <thiagohenrique776@gmail.com>
2021-04-26 15:23:27 -0300

    API routes e parametrização nas rotas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { privateDecrypt } from "node:crypto";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -40,9 +40,9 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IjThxG8doYgVWDuBdi9qQSJ')
-  
+
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 Hours
   }
 }

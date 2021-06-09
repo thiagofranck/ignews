201ebf7dce60ad9eefdf66e2d82d672a535f1571
Thiago Franck <thiagohenrique776@gmail.com>
2021-06-09 14:16:38 -0300

    Test componente SignInButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,15 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn,signOut, useSession } from 'next-auth/client'
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const [session] = useSession()
+  const [session] = useSession();
 
   return session ? (
-    <button 
-      type="button" 
+    <button
+      type="button"
       className={styles.signInButton}
       onClick={() => signOut()}
     >
@@ -18,10 +18,10 @@ export function SignInButton() {
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button 
-      type="button" 
-      className={styles.signInButton} 
-      onClick={() => signIn('github')}
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
     >
       <FaGithub color="#EBA417" />
       Sign in with Github
